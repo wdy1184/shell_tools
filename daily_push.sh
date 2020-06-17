bfe483196b9fd118795c5a3f21b673b9a29a976c
wudanyang <wudanyang@baidu.com>
2020-06-17 10:10:00 +0800

    daily_push

--- daily_push.sh
+++ daily_push.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # 同步 git
 syncGit() {
     git pull
@@ -11,17 +12,19 @@ syncGit() {
     git ci -m 'daily_push'
     git push
 }
+cd /Users/wudanyang/workspace/shell_tools
+syncGit
+#cd ../
+#base_dir=`pwd`
 
-cd ../
-base_dir=`pwd`
 
 # 包含 study 或者是 dot_file 文件夹都提交
-dirs=`ls -l | grep -E "study|dot_file|tools" | awk '{print $9;}'`
-for dir in $dirs
-do
-    echo '+++++'
-    cd ${base_dir}/${dir}
-    pwd
-    syncGit
-    echo '+++++'
-done
+#dirs=`ls -l | grep -E "study|dot_file|tools" | awk '{print $9}'`
+#for dir in $dirs
+#do
+#    echo '+++++'
+#    cd ${base_dir}/${dir}
+#    pwd
+#    syncGit
+#    echo '+++++'
+#done
